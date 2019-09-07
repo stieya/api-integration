52b500b2cc694c9b020d1fcb1fc4a121ee649e83
andrewtooyut <andrew.tooy@uangteman.com>
2019-09-08 04:05:27 +0700

    change sqlx into interface

--- api_integration.go
+++ api_integration.go
@@ -13,15 +13,12 @@ import (
 	"time"
 
 	"github.com/spartaut/activity"
-
-	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
 )
 
 type APIIntegration struct {
 	UserID      int64
 	Token       string
-	APIName     string
+	APICode     string
 	Method      string
 	ContentType string
 	Host        string
@@ -32,15 +29,15 @@ type APIIntegration struct {
 	Logger      *log.Logger
 }
 
-func (a *APIIntegration) Send(db *sqlx.DB) (interface{}, error) {
+func (a *APIIntegration) Send(db interface{}) (interface{}, error) {
 	var resp interface{}
 
 	// generate http request
 	client := http.Client{Timeout: a.validateTimeout() * time.Second}
 	req, err := http.NewRequest(a.Method, a.Host, a.generateBody())
 	if err != nil {
-		go a.writeLog("[" + a.APIName + "] - Failed Send - new request - Error: " + err.Error())
-		log.Println("[", a.APIName, "] - Failed new request - ", err.Error())
+		go a.writeLog("[" + a.APICode + "] - Failed Send - new request - Error: " + err.Error())
+		log.Println("[", a.APICode, "] - Failed new request - ", err.Error())
 		return nil, err
 	}
 
@@ -48,26 +45,26 @@ func (a *APIIntegration) Send(db *sqlx.DB) (interface{}, error) {
 	a.generateHeaders(req)
 
 	// set api activity
-	apiActivity := activity.NewAPIActivityRequest(a.UserID, a.Token, time.Now().Format("2006-01-02 15:04:05"), a.APIName, req)
+	apiActivity := activity.NewAPIActivityRequest(a.UserID, a.Token, time.Now().Format("2006-01-02 15:04:05"), a.APICode, req)
 
 	// post to idm with send apiactivity
 	response, err := apiActivity.ClientDo(db, client, req)
 	if err, ok := err.(net.Error); ok && err.Timeout() {
-		go a.writeLog("[" + a.APIName + "] - Failed Send - Error: " + err.Error())
-		log.Println("[", a.APIName, "] - Failed timeout - ", err.Error())
+		go a.writeLog("[" + a.APICode + "] - Failed Send - Error: " + err.Error())
+		log.Println("[", a.APICode, "] - Failed timeout - ", err.Error())
 		return nil, errors.New("timeout api")
 	}
 	if err != nil {
-		go a.writeLog("[" + a.APIName + "] - Failed Send - post - Error: " + err.Error())
-		log.Println("[", a.APIName, "] - Failed post - ", err.Error())
+		go a.writeLog("[" + a.APICode + "] - Failed Send - post - Error: " + err.Error())
+		log.Println("[", a.APICode, "] - Failed post - ", err.Error())
 		return nil, err
 	}
 
 	// read response from idm
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		go a.writeLog("[" + a.APIName + "] - Failed Send - read response body - Error: " + err.Error())
-		log.Println("[", a.APIName, "] - Failed read response body - ", err.Error())
+		go a.writeLog("[" + a.APICode + "] - Failed Send - read response body - Error: " + err.Error())
+		log.Println("[", a.APICode, "] - Failed read response body - ", err.Error())
 		return nil, err
 	}
 
@@ -76,8 +73,8 @@ func (a *APIIntegration) Send(db *sqlx.DB) (interface{}, error) {
 
 	err = json.Unmarshal(body, &resp)
 	if err != nil {
-		go a.writeLog("[" + a.APIName + "] - Failed Send - unmarshal response body - Error: " + err.Error())
-		log.Println("[", a.APIName, "] - Failed unmarshal response body - Error: ", err.Error())
+		go a.writeLog("[" + a.APICode + "] - Failed Send - unmarshal response body - Error: " + err.Error())
+		log.Println("[", a.APICode, "] - Failed unmarshal response body - Error: ", err.Error())
 		return nil, err
 	}
 
